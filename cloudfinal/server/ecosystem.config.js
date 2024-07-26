1b81acdd1e9a329e42981d228148dd0c881be108
JBharadwaj <bhardwajjangam@gmail.com>
2024-07-26 19:42:20 +0530

    third commit after making changes for concurrently and replaced by ampersand symbol

---
+++ cloudfinal/server/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+      {
+        name: "server",
+        script: "./server.js",
+        watch: true
+      },
+      {
+        name: "trial",
+        script: "./trial.js",
+        watch: true
+      }
+    ]
+  }
+  
